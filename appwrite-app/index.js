fa64e3bfbbee6e663d5738644031e2395c943c7c
shivchawla <contact@mositechnologies.com>
2024-08-12 18:26:25 -0400

    Some comments

--- appwrite-app/index.js
+++ appwrite-app/index.js
@@ -1,6 +1,6 @@
 const generateJwtImpl = require('./functions/generateJwt')
-console.log(generateJwtImpl)
 
+//Create a mapper entry for each function
 const FUNCTION_MAPPER = {"FUNCTION_ID": generateJwtImpl}
 
 //Mocking createExecution. this will directly call the function 
@@ -8,8 +8,9 @@ const createExecution = async (functionId, body, async = false, xpath='', method
     console.log({functionId, body, async, xpath, method})
     try {
         return FUNCTION_MAPPER[functionId]({
-            req: {body, method}, 
+            req: {body, method, headers:{}}, 
             res:{
+                // This may be incorrect
                 json: async(obj) => {
                     console.log(obj)
                     return {response: JSON.stringify(obj)}
--- nextjs-app/src/app/api/appwriteExecution.js
+++ nextjs-app/src/app/api/appwriteExecution.js
@@ -10,9 +10,15 @@ client
 
 
 export default async function appwriteExecution(input) {
+    //If Production, it will go to server on appwrite via network
     if (process.env.NODE_ENV == 'production') {
         return functions.createExecution('FUNCTION_ID', JSON.stringify({ input }));
     } else {
+        // If run on development mode,
+        // It will use local executor, function will get executed existing server
+        // Use npm link ../appwrite-app
+        // This brings appwriteExecutionHelper.createExecution local helper. See implementation for details
+        // This avoid deployments and can be used for quicker development.
         console.log("Running in development env")
         return appwriteExecutionHelper.createExecution('FUNCTION_ID', JSON.stringify({ input }))
     }
